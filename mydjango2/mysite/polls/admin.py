86a4ac3fef504e22e9d15b6e111d098ad3a152df
JungwooKim1271011706 <wjddn6269@gamil.com>
2020-02-08 16:00:14 +0900

    mydjango2

---
+++ mydjango2/mysite/polls/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from polls.models import Question, Choice
+# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+    
+class QuestionAdmin(admin.ModelAdmin):
+    # field = ['pub_date','question_text'] 필드 순서 변경하기
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date Information', {'fields':['pub_date'], 'classes':['collapse']}), #필드 나누기 및 필드 접기 기능 추가
+    ]
+    inlines = [ChoiceInline] #Choice 모델 클래스 같이 보기
+    list_display = ('question_text', 'pub_date') #레코드 리스트 컬럼 지정
+    list_filter = ['pub_date'] # 시간 필터
+    search_fields = ['question_text'] # 검색 기능 추가
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
---
+++ mydjango2/mysite/polls/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self): # 주소값이 아니라 텍스트 값을 출력함
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+    #외래키(ForeignKey) - 질문항목을 받아서 선택지를 만듦//질문(1):선택지(N)의 관계를 형성한다
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+        
