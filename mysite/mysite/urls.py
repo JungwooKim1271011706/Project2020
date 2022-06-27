a42d68bf3da8f68bbb7f7956faf55b66782958d0
esji <esji@wemakeprice.com>
2022-06-27 18:21:09 +0900

    음 .. 아이디 잘 들어가니?

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from mysite import views
-from django.contrib.auth import views as auth_views
+#from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
